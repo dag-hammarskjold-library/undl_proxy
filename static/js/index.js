2c91ab70598f3137c47c8b9889b633ced3af12c4
Andrew Lee <fiacre.patrick@gmail.com>
2017-12-20 10:29:51 -0500

    get checkbox toggle working

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
     
-    $('.check:button').toggle(function(){
-        $('input:checkbox').attr('checked','checked');
-        $(this).val('uncheck all');
-    },function(){
-        $('input:checkbox').removeAttr('checked');
-        $(this).val('check all');        
-    })
+    $("#checkAll").change(function () {
+        $("input:checkbox").prop('checked', $(this).prop("checked"));
+    });
 
 });
